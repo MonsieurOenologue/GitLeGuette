4f76f243ddf9aa18f9c18b84fbc1c5e847ae4f07
MonsieurOenologue <thomas.lfbvr@gmail.com>
2016-03-22 16:11:30 +0100

    initial push

---
+++ minish.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "minish.h"
+
+//printf le résultat d'un ls
+
+
+
+//thread bonjour
+
+
+//Algo
+//while not EOF(stdin)
+//do
+//    read(cmd);
+//    fork();
+//    child: exec(cmd);
+//    parent : wait (child)
