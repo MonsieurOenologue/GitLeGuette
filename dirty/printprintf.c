83fd1bbc56b6312d8a08a1f5b2cf3347fa181df7
uj <aulien.jbadji@gmail.com>
2016-03-22 17:57:32 +0100

    odifahif

--- dirty/printprintf.c
+++ dirty/printprintf.c
@@ -1,6 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
 int main() {
-    system("ls ~");
-    return 0;
+  // char data[] = "ls ~";
+  // int ret = exec(data);
+  // //ls("/home/uj/");
+  // printf("%d", ret);
+  // return 0;
+  char data[] = "ls -la /home/uj";
+  test_exec(data);
 }
+
+/*
+  Exec est une callback
+*/
+//askip c'est pas nécessaire.
+int ls(char *dir) {
+  int len;
+  struct dirent *pDirent;
+  DIR *pDir;
+
+  pDir = opendir (dir);
+  while ((pDirent = readdir(pDir)) != NULL) {
+            printf ("[%s]\n", pDirent->d_name);
+        }
+        closedir (pDir);
+        return 0;
+}
+
+int test_exec(char *data) {
+  //char data[] = "ls -larsh /home/uj/dev/";
+  //LET'S COUNT PARAMETERS
+  int argc = 1;
+  char *ptr = data;
+  char *tok;
+  int i=0;
+  while((ptr = strchr(ptr, ' ')) != NULL) {
+    argc++;
+    ptr++;
+  }
+  //printf("%d\n", argc);
+  char **parsed = malloc((argc+1)*sizeof(char));
+  tok = strtok(data, " ");
+  while(tok != NULL) {
+    //printf("%s\n", tok);
+    parsed[i] = strdup(tok);
+    tok = strtok(NULL, " ");
+    i++;
+  }
+  parsed[argc] = NULL;
+  for(i=0; i<argc; i++) {
+    printf("%s\n", parsed[i]);
+  }
+  //char *parsed[] = {"ls", "-la", "/home/uj", NULL};
+  printf("%d\n", execvp(parsed[0], parsed));
+  //printf("%d\n", execlp(parsed_list, parsed_list, 0));
+
+  return 1;
+}
+// int exec(char *prompt) {
+//   //char *arguments[] = argv;
+//   //char *supertoast = "ls";
+//   printf("%s", prompt);
+//   //char data[] = "ls -la";
+//   char *command = (char*) strtok(prompt, " ");
+//   while(command != NULL) {
+//     printf("%s\n", command);
+//     command = strtok(command, " ");
+//   }
+  //execlp(command, command, 0);
+  //printf("%s", command);
+//  return 1;
+//}
+/*int exec(char *command) {
+  char *arguments[] = {command, NULL};
+  return execvp(arguments[0], arguments);
+}*/
